c7cc94d3df360e18018ce7303fbc433f24477e85
jennyvaque01-spec <jennyvaque01@gmail.com>
2026-05-27 18:31:07 -0500

    feat(application): agregar interfaz- service and request

---
+++ UdemyClone.Application/Interfaces/IListaDeseoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyClone.Application.Models.DTOs;
+using UdemyClone.Application.Models.Requests.Estudiante;
+
+namespace UdemyClone.Application.Interfaces
+{
+    public interface IListaDeseoService
+    {
+        Task<List<ListaDeseoDto>> GetByEstudiante(int estudianteId);
+        Task<ListaDeseoDto> Create(CreateListaDeseoRequest model);
+        Task<bool> Delete(int cursoId, int estudianteId);;
+    }
+}
---
+++ UdemyClone.Application/Interfaces/UpdateEstudianteService.cs
@@ -0,0 +1,9 @@
+﻿using UdemyClone.Application.Models.Requests.Estudiante;
+
+namespace UdemyClone.Application.Interfaces
+{
+    public interface UpdateEstudianteService
+    {
+        Task<bool> CreateEstudianteAsync(CreateEstudianteRequest request);
+    }
+}
