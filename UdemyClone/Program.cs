2ed36afe2fd3bbcaff9b5a70bab8c3bef022c1bd
jennyvaque01-spec <jennyvaque01@gmail.com>
2026-05-27 20:19:24 -0500

    Feat(webapi): Implementacion de controladores y endpoints

--- UdemyClone/Program.cs
+++ UdemyClone/Program.cs
@@ -10,12 +10,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<UdemyCloneContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+//servicios
 builder.Services.AddScoped<IEstudianteService, EstudianteService>();
+builder.Services.AddScoped<IInstructorService, InstructorService>();
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ICursoService, CursoService>();
+builder.Services.AddScoped<ISeccionService, SeccionService>();
+builder.Services.AddScoped<ILeccionService, LeccionService>();
+builder.Services.AddScoped<IInscripcionService, InscripcionService>();
+builder.Services.AddScoped<IResenaService, ResenaService>();
+builder.Services.AddScoped<IListaDeseoService, ListaDeseoService>();
+builder.Services.AddScoped<IProgresoService, ProgresoService>();
+
+//repositorios
 builder.Services.AddScoped<IEstudianteRepository, EstudianteRepository>();
+builder.Services.AddScoped<IInstructorRepository, InstructorRepository>();
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<ICursoRepository, CursoRepository>();
+builder.Services.AddScoped<ISeccionRepository, SeccionRepository>();
+builder.Services.AddScoped<ILeccionRepository, LeccionRepository>();
+builder.Services.AddScoped<IInscripcionRepository, InscripcionRepository>();
+builder.Services.AddScoped<IResenaRepository, ResenaRepository>();
+builder.Services.AddScoped<IListaDeseoRepository, ListaDeseoRepository>();
+builder.Services.AddScoped<IProgresoRepository, ProgresoRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -29,6 +56,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+app.UseCors("AllowAngular");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
